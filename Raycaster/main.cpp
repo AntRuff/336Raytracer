66f80c236f4e76d19a3f88a5626f6047f1e2be76
Bt2solomon <44513630+Bt2solomon@users.noreply.github.com>
2021-10-12 01:24:30 -0500

    made a tiny bit of progress w specular I guess

--- Raycaster/main.cpp
+++ Raycaster/main.cpp
@@ -89,15 +89,15 @@ public:
 
     hit getHit(vec3 rayStart, vec3 rayEnd) override {
         double a = pow((rayEnd.x() - rayStart.x()), 2)
-                  + pow((rayEnd.y() - rayStart.y()), 2)
-                  + pow((rayEnd.z() - rayStart.z()), 2);
+                   + pow((rayEnd.y() - rayStart.y()), 2)
+                   + pow((rayEnd.z() - rayStart.z()), 2);
         double b = 2 * (((rayEnd.x() - rayStart.x())*(rayStart.x()-origin.x()))
-                       +((rayEnd.y() - rayStart.y())*(rayStart.y()-origin.y()))
-                       +((rayEnd.z() - rayStart.z())*(rayStart.z()-origin.z())));
+                        +((rayEnd.y() - rayStart.y())*(rayStart.y()-origin.y()))
+                        +((rayEnd.z() - rayStart.z())*(rayStart.z()-origin.z())));
         double c = pow(origin.x(), 2) + pow(origin.y(), 2) + pow(origin.z(), 2)
-                  + pow(rayStart.x(), 2) + pow(rayStart.y(), 2) + pow(rayStart.z(), 2)
-                  - 2 * ((origin.x()*rayStart.x()) + (origin.y()*rayStart.y()) + (origin.z()*rayStart.z()))
-                  - pow(radius, 2);
+                   + pow(rayStart.x(), 2) + pow(rayStart.y(), 2) + pow(rayStart.z(), 2)
+                   - 2 * ((origin.x()*rayStart.x()) + (origin.y()*rayStart.y()) + (origin.z()*rayStart.z()))
+                   - pow(radius, 2);
 
         double intersect = (b * b) - (4 * a * c);
 
@@ -108,8 +108,8 @@ public:
                              rayStart.y() + u*(rayEnd.y() - rayStart.y()),
                              rayStart.z() + u*(rayEnd.z() - rayStart.z()));
             double tempD = sqrt(pow(temp.x() - rayStart.x(), 2) +
-                               pow(temp.y() - rayStart.y(), 2) +
-                               pow(temp.z() - rayStart.z(), 2));
+                                pow(temp.y() - rayStart.y(), 2) +
+                                pow(temp.z() - rayStart.z(), 2));
             return hit(tempD, temp, (temp - origin) / (temp - origin).length(), color, matKey);
         } else {
             double uP = (-b + sqrt(intersect))/(2*a);
@@ -121,11 +121,11 @@ public:
                               rayStart.y() + uN*(rayEnd.y() - rayStart.y()),
                               rayStart.z() + uN*(rayEnd.z() - rayStart.z()));
             double tempPD = sqrt(pow(tempP.x() - rayStart.x(), 2) +
-                                pow(tempP.y() - rayStart.y(), 2) +
-                                pow(tempP.z() - rayStart.z(), 2));
+                                 pow(tempP.y() - rayStart.y(), 2) +
+                                 pow(tempP.z() - rayStart.z(), 2));
             double tempND = sqrt(pow(tempN.x() - rayStart.x(), 2) +
-                                pow(tempN.y() - rayStart.y(), 2) +
-                                pow(tempN.z() - rayStart.z(), 2));
+                                 pow(tempN.y() - rayStart.y(), 2) +
+                                 pow(tempN.z() - rayStart.z(), 2));
             if (tempPD < tempND) {
                 return hit(tempPD, tempP, (tempP - origin) / (tempP - origin).length(), color, matKey);
             }
@@ -134,6 +134,47 @@ public:
         return hit(-1, vec3(-1, -1, -1), vec3(-1, -1, -1), color, matKey);
     }
 };
+
+//class triangle : public shape {
+//public:
+//    vec3 t0;
+//    vec3 t1;
+//    vec3 t2;
+//
+//    triangle (vec3 a, vec3 b, vec3 c, vec3 color, double mk){
+//        t0 = a;
+//        t1 = b;
+//        t2 = c;
+//        this->color = color;
+//        matKey = mk;
+//    }
+//
+//    hit getHit(vec3 rayStart, vec3 rayEnd) override {
+//
+//        vec3 edge0 = t1 - t0;
+//        vec3 edge1 = t2 - t0;
+//        vec3 edge2 = t2 - t1;
+//        vec3 edge3 = t0 - t2;
+//
+//        vec3 n = edge0.cross(edge1) / (edge0.cross(edge1)).length();
+//        double d = n.dot(t0);
+//
+//        if (n.dot(rayEnd) == 0) return hit(-1, vec3(-1, -1, -1), vec3(-1, -1, -1), color, matKey);
+//
+//        double t = (d - n.dot(rayStart)) / n.dot(rayEnd);
+//
+//        vec3 q = rayStart + t * rayEnd;
+//
+//        vec3 c0 = q - t0;
+//        vec3 c1 = q - t1;
+//        vec3 c2 = q - t2;
+//
+//        if (n.dot(edge0.cross(c0)) >= 0 && n.dot(edge2.cross(c1)) >= 0 && n.dot(edge3.cross(c2)) >= 0) {
+//            return hit(t, q, n, color, matKey);
+//        }
+//        else return hit(-1, vec3(-1, -1, -1), vec3(-1, -1, -1), color, matKey);
+//    }
+//};
 
 class triangle : public shape {
 public:
@@ -148,6 +189,38 @@ public:
         this->color = color;
         matKey = mk;
     }
+
+//    hit getHit(vec3 rayStart, vec3 rayEnd) override {
+//        vec3 edge0 = t1-t0;
+//        vec3 edge1 = t2-t1;
+//        vec3 edge2 = t0-t2;
+//        vec3 normal = (t2-t0).cross(edge0);
+////        vec3 normal = edge0.cross((t2-t0));
+//        normal = normal / normal.length();
+//        vec3 dir = rayEnd-rayStart;
+//        dir = dir / dir.length();
+//
+//
+//        if (normal.dot(dir) == 0) return hit(-1, vec3(-1, -1, -1), vec3(-1, -1, -1), color, matKey);
+//
+////        double dis = 0.75;
+//        double dis = abs(normal.dot(rayStart - t0));
+////        double t = ((dis - normal.dot(rayStart))/ normal.dot(dir));
+//        double t = ((normal.dot(rayStart) + dis)/ normal.dot(dir));
+//        if (t < 0) return hit(-1, vec3(-1, -1, -1), vec3(-1, -1, -1), color, matKey);
+//        vec3 p = t * dir;
+////        vec3 p = rayStart + t * dir;
+//        vec3 c0 = p - t0;
+//        vec3 c1 = p - t1;
+//        vec3 c2 = p - t2;
+//
+//        double distance = sqrt(pow(p.x()-rayStart.x(), 2) + pow(p.y()-rayStart.y(), 2) + pow(p.z()-rayStart.z(), 2));
+//
+//        if (normal.dot(edge0.cross(c0)) >= 0 && normal.dot(edge1.cross(c1)) >= 0 && normal.dot(edge2.cross(c2)) >= 0) {
+//            return hit(t, p, normal, color, matKey);
+//        }
+//        else return hit(-1, vec3(-1, -1, -1), vec3(-1, -1, -1), color, matKey);
+//    }
 
     hit getHit(vec3 rayStart, vec3 rayEnd) override {
         vec3 edge0 = t1-t0;
@@ -181,8 +254,8 @@ hit Raycast(vec3 rayStart, vec3 rayEnd, list<shape*> shapeList){
 
     vec3 closestPoint = vec3(FLT_MAX, FLT_MAX, FLT_MAX);
     double closestD = sqrt(pow(closestPoint.x() - rayStart.x(), 2) +
-                          pow(closestPoint.y() - rayStart.y(), 2) +
-                          pow(closestPoint.z() - rayStart.z(), 2));
+                           pow(closestPoint.y() - rayStart.y(), 2) +
+                           pow(closestPoint.z() - rayStart.z(), 2));
 //    vec3 color = vec3(50, 50, 50);
 
     hit h = hit(-1, vec3(-1, -1, -1), vec3(-1, -1, -1), vec3(-1, -1, -1), -1);
@@ -195,11 +268,11 @@ hit Raycast(vec3 rayStart, vec3 rayEnd, list<shape*> shapeList){
 
         }
         else if ((rayStart.x() - rayEnd.x() >= 0 && rayStart.x() - tempH.loc.x() < 0) ||
-                 (rayStart.x() - rayEnd.x() < 0 && rayStart.x() - tempH.loc.x() >= 0) ||
+                 (rayStart.x() - rayEnd.x() <= 0 && rayStart.x() - tempH.loc.x() > 0) ||
                  (rayStart.y() - rayEnd.y() >= 0 && rayStart.y() - tempH.loc.y() < 0) ||
-                 (rayStart.y() - rayEnd.y() < 0 && rayStart.y() - tempH.loc.y() >= 0) ||
+                 (rayStart.y() - rayEnd.y() <= 0 && rayStart.y() - tempH.loc.y() > 0) ||
                  (rayStart.z() - rayEnd.z() >= 0 && rayStart.z() - tempH.loc.z() < 0) ||
-                 (rayStart.z() - rayEnd.z() < 0 && rayStart.z() - tempH.loc.z() >= 0)) {
+                 (rayStart.z() - rayEnd.z() <= 0 && rayStart.z() - tempH.loc.z() > 0)) {
 
         }
         else if (d < closestD){
@@ -256,8 +329,8 @@ int main(int argc, char *agrv[]) {
 
     shape* s1 = new sphere(vec3(0.25, 1, 2), 0.5, vec3(0., 0., 1), 1);
     shape* s2 = new sphere(vec3(1.75, 1, 2), 0.5, vec3(1, 0., 0.), 1);
-    shape* s3 = new sphere(vec3(1, 0.5, 4), 1, vec3(1, 1, 1), 2);
-    shape* t1 = new triangle(vec3(1, 1.5, 0), vec3(1-10, 1.5, 10), vec3(11, 1.5, 10), vec3(0.5, 1, 0.5), 1);
+    shape* s3 = new sphere(vec3(1, 0.5, 4), 1, vec3(0.5, 0.5, 0.5), 2);
+    shape* t1 = new triangle(vec3(1, 1.5, 0), vec3(11, 1.5, 10), vec3(-9, 1.5, 10), vec3(0.5, 0.5, 0.5), 1);
 
 //    s1;
 //    s2;
@@ -274,7 +347,7 @@ int main(int argc, char *agrv[]) {
     for (y = 0; y < yBound; y++){
         for (x = 0; x < xBound; x++){
             vec3 color = vec3(0, 0, 0);
-            if (x == 360 && y == 260) {
+            if (x == 150 && y == 280) {
                 printf("");
             }
             for (s = 0; s < samples; s++) {
